7cede282d03b2ced39e7ab9ccb432b259f9b4f6f
Gemma Nash <nashgemm@grinnell.edu>
2017-10-24 15:09:54 +0000

    set up for devise, user authentication

--- volunteer-site/db/migrate/20171007001142_rename_column.rb
+++
@@ -1,5 +0,0 @@
-class RenameColumn < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :interests, :user, :user_id
-  end
-end
--- volunteer-site/db/migrate/20171007004731_add_index_to_users_phone.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToUsersPhone < ActiveRecord::Migration[5.1]
-  def change
-    add_index :users, :phone, unique: true
-  end
-end
