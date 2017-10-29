04706d79a8fe371e34784ba48d70134110f99e9e
Clara Bertaut <bertautc17@grinnell.edu>
2017-10-29 20:06:46 +0000

    TRYING to push to heroku

--- volunteer-site/app/assets/javascripts/application.js
+++ volunteer-site/app/assets/javascripts/application.js
@@ -10,7 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
 //= require kickstart
 //= require rails-ujs
 //= require turbolinks
+//= require maskedinput
 //= require_tree .
