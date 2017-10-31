40a5a68ec69f2b3d5c3bbeeacee936813ba8dbbc
Gemma Nash <nashgemm@grinnell.edu>
2017-10-31 16:12:38 +0000

    debug auth-system

--- volunteer-site/config/routes.rb
+++ volunteer-site/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
-  devise_for :users, ActiveAdmin::Devise.config
+  devise_for :users
+  devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   
   get  '/signup',  to: 'users#new'
