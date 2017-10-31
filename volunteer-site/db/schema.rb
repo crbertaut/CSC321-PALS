4515f1bdade3252370a0616dcd9adc59a58b8beb
JaeEun <ohjaeeun09@gmail.com>
2017-10-31 15:04:39 +0000

    community board now working

--- volunteer-site/db/schema.rb
+++ volunteer-site/db/schema.rb
@@ -5,21 +5,12 @@
 # Note that this schema.rb definition is the authoritative source for your
 # database schema. If you need to create the application database on another
 # system, you should be using db:schema:load, not running all the migrations
-# from scratch. The +latter is a flawed and unsustainable approach (the more migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024143516) do
-  
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.string   "type"
-    t.text     "description"
-    t.datetime "date"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20171024152023) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -33,6 +24,22 @@ ActiveRecord::Schema.define(version: 20171024143516) do
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+  end
+
+  create_table "interests", force: :cascade do |t|
+    t.string "name"
+    t.string "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "type"
+    t.text "description"
+    t.datetime "date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
