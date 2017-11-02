b91c9d92a657073f80672d13888a54da09757835
Gemma Nash <nashgemm@grinnell.edu>
2017-11-02 14:27:09 +0000

    user: user@example.com || password: userPassword // // // link: c9-url/users/confirmation?confirmation_token=token (copy the token from the admin page)

---
+++ volunteer-site/app/admin/user.rb
@@ -0,0 +1,22 @@
+ActiveAdmin.register User do
+    permit_params :email, :password, :password_confirmation
+    
+    index do
+        selectable_column
+        id_column
+        column :email
+        actions
+    end
+    
+    filter :email
+    filter :created_at
+
+    form do |f|
+        f.inputs do
+            f.input :email
+            f.input :password
+            f.input :password_confirmation
+        end
+        f.actions
+    end
+end
