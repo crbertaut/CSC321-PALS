300bdd6841fce12b2d25e19f9195054b6d798184
Gemma Nash <nashgemm@grinnell.edu>
2017-11-07 08:42:31 +0000

    finish customizing dash

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,10 +3,24 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "Recently registered voluneteers" do
+          User.all.order(created_at: :desc).each do |user|
+            li user.email
+            li user.created_at
+          end
+        end
+      end
+      
+      column do
+        panel "Recent posts" do
+          Post.all.order(created_at: :desc).each do |post|
+            li post.title
+            li post.date
+            li post.thread_type
+          end
+        end
       end
     end
 
