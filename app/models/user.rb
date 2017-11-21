488a818ba053c0aea4fd175444a23e4bceffc4bf
Gemma Nash <nashgemm@grinnell.edu>
2017-11-21 01:02:34 +0000

    Test

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
         # :confirmable
          
-<<<<<<< HEAD
   has_many :posts
-=======
   has_and_belongs_to_many :interests
->>>>>>> cfd7d98ba2e62bf86b97cd933615e0f658b590a8
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20171113035739) do
-=======
 ActiveRecord::Schema.define(version: 20171116023030) do
->>>>>>> cfd7d98ba2e62bf86b97cd933615e0f658b590a8
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -57,8 +53,8 @@ ActiveRecord::Schema.define(version: 20171116023030) do
   create_table "interests_users", id: false, force: :cascade do |t|
     t.integer "interest_id", null: false
     t.integer "user_id", null: false
-    t.index ["interest_id", "user_id"], name: "index_interests_users_on_interest_id_and_user_id"
-    t.index ["user_id", "interest_id"], name: "index_interests_users_on_user_id_and_interest_id"
+    t.index ["interest_id"], name: "index_interests_users_on_interest_id"
+    t.index ["user_id"], name: "index_interests_users_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
