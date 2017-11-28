4ccb72c21453d91e845fedaa3c46fa6b246a0e97
Ella Nicolson <nicolson@grinnell.edu>
2017-11-28 16:44:33 +0000

    fixing styling heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,20 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20171126014148) do
 
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.integer "resource_id"
-    t.string "author_type"
-    t.integer "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
-  end
-
   create_table "admin_users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -43,20 +29,10 @@ ActiveRecord::Schema.define(version: 20171126014148) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.integer "post_id"
-    t.integer "user_id"
-    t.text "message"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
   create_table "interests", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "interests_users", id: false, force: :cascade do |t|
@@ -107,7 +83,6 @@ ActiveRecord::Schema.define(version: 20171126014148) do
     t.string "name"
     t.string "phone"
     t.date "dob"
-    t.text "interests"
     t.text "experience"
     t.text "username"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
