94880b94f2f122372266ae931d945594f77564c6
Caelin Bryant <bryantca@grinnell.edu>
2017-11-30 15:42:50 +0000

    Fixed routing for profile button

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: :index
   
   def post_params
-    params.require(:post).permit(:title, :thread_type, :date, :description, :user_id)
+    params.permit(:post, :title, :thread_type, :date, :description, :user_id)
   end
 
   def show
