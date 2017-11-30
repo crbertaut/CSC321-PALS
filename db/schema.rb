38baba7eab34927af7121f08f8f55e09b06dbaed
Caelin Bryant <bryantca@grinnell.edu>
2017-11-30 15:06:23 +0000

    profile button

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20171126014148) do
+=======
+ActiveRecord::Schema.define(version: 20171116165422) do
+>>>>>>> profile button
 
   create_table "admin_users", force: :cascade do |t|
     t.string "email", default: "", null: false
