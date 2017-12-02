e204a476146ad581bc800e9586b0639a32b6c3f7
Ella Nicolson <nicolson@grinnell.edu>
2017-12-02 18:15:39 +0000

    Fix registration controller/view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format.json? }
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
