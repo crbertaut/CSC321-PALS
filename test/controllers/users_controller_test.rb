b9cd44e00738ee8eb5e4af9ba9b3b17d3661d56b
Mattori Birnbaum <birnbaum@grinnell.edu>
2018-02-23 21:52:29 +0000

    added volunteer index tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -8,20 +8,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @user = create(:user)
   end
   
-  test "should ask for username and password" do
+  test "login should ask for username and password" do
     get new_user_session_path
     assert_response :success
     assert_select "input#user_username"
     assert_select "input#user_password"
   end
   
-  test "should force login when viewing profile" do
+  test "show should force login" do
     get user_path @user.id
     assert_response :redirect
     assert_redirected_to new_user_session_path
   end
   
-  test "should list profile information" do
+  test "show should list profile information" do
     sign_in @user
     get user_path @user.id
     assert_response :success
@@ -32,5 +32,24 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_select "#interests", @user.interests.map(&:name).join(", ")
     assert_select "#phone", @user.phone
     assert_select "#email", @user.email
+  end
+  
+  test "index should force login" do
+    get users_path
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+  
+  test "index should list user info" do
+    sign_in @user
+    get users_path
+    assert_response :success
+    assert_select "#volunteers tbody tr", 1
+    assert_select "#volunteers tbody tr" do
+      assert_select "td", 3 
+      assert_select "td:nth(1)", @user.username
+      assert_select "td:nth(2)", @user.name
+      assert_select "td:nth(3)", @user.created_at.strftime("%b %e, %Y")
+    end
   end
 end
