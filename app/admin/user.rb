3e939512a8c0a4c60cfe233faed5241dd089076d
Mattori Birnbaum <birnbaum@grinnell.edu>
2018-04-06 20:24:19 +0000

    updated old references to email to home_email

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register User, as: 'Volunteer' do
     permit_params do
-        params = [:email, :name, :phone, :dob, :password, :password_confirmation, :other_interests, interest_ids:[]]
+        params = [:home_email, :name, :phone, :dob, :password, :password_confirmation, :other_interests, interest_ids:[]]
         params
     end
 
@@ -10,7 +10,7 @@ ActiveAdmin.register User, as: 'Volunteer' do
         selectable_column
         id_column
         column :name
-        column :email
+        column :home_email
         column :phone
         column "Interests" do |user|
             user.interests.collect(&:name)
@@ -19,7 +19,7 @@ ActiveAdmin.register User, as: 'Volunteer' do
     end
     
     filter :name
-    filter :email
+    filter :home_email
     filter :created_at
     filter :interests, collection: proc { Interest.all }
     
@@ -31,7 +31,7 @@ ActiveAdmin.register User, as: 'Volunteer' do
                 f.input :password_confirmation
             end
             f.input :email, required: true
-            f.input :phone
+            f.input :home_phone
             f.input :dob, as: :date_select, :start_year => 1900, :end_year => Date.current.year, :use_month_numbers => true, :order => [:year, :month, :day]
             f.input :interests, as: :check_boxes
             f.input :other_interests, as: :string
@@ -72,7 +72,7 @@ ActiveAdmin.register User, as: 'Volunteer' do
     show do
         attributes_table title: "Profile" do
           row :name
-          row :email
+          row :home_email
           row :phone
           row :dob
           row "Interests" do |user|
