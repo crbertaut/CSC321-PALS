390998ccd638862bc6230ec544fc67db975abfab
Mattori Birnbaum <birnbaum@grinnell.edu>
2018-05-02 19:03:34 +0000

    added shifts to second seed user

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,6 +28,10 @@ if Rails.env.development?
     user2.interests << (Interest.find_by name: 'Cooking/baking')
     user2.interests << (Interest.find_by name: 'Machine maintenance')
     
+    user2.shifts.create!(kind: 0, start: "2018-04-05 15:00:00", finish: "2018-04-05 15:30:00")
+    user2.shifts.create!(kind: 0, start: "2018-04-07 15:00:00", finish: "2018-04-07 15:30:00")
+    user2.shifts.create!(kind: 0, start: "2018-04-07 17:00:00", finish: "2018-04-07 18:30:00")
+    
     User.create!(name: 'Virginia Potts', password: 'babydonthurtme', home_email: 'whatislove@babydonthurtme.com', home_phone: '', dob: '1976-03-14')
     User.create!(name: 'Jane Foster', password: 'science!', home_email: 'fostersaucester@science.com', home_phone: '(123) 456-7890', dob: '1989-04-16')
     User.create!(name: 'Marina Diamondis', password: 'iamnotarobot', home_email: 'diamondis@marinara.com', home_phone: '', dob: '1992-06-30')
